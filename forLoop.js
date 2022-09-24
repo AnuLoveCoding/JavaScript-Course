3510a263003be4ca4971fd465e53fb0c3635220e
anuLoveCoading <anusrivas07@gmail.com>
2022-09-24 13:42:03 +0530

    first commit

---
+++ forLoop.js
@@ -0,0 +1,13 @@
+// console.log("hello Anurag");
+
+
+// for (let i=1;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+for (var j=100;j<=1;j--){
+    
+    console.log(j);
+}
