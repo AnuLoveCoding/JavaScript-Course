3692bb792852882ceacd4b9e01a581bcdee36c59
anuLoveCoading <anusrivas07@gmail.com>
2022-09-24 22:24:15 +0530

    class

--- demo.java
+++ demo.java
@@ -6,12 +6,18 @@ public class demo {
     public static void main(String[] args) {
         
 
-        int num =20;
-        System.out.println(num+30);
+//         int num =20;
+//         // System.out.println(num+30);
 
-         num =50;
- System.out.println(num);
+//          num =50;
+//  System.out.println(num);
 
+float new_number = 10.35f;
+  System.out.println(new_number);
+
+
+  double Aew_number = 10.36;
+  System.out.println(Aew_number);
 
 
     }
