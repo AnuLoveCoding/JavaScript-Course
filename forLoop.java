943f492a5bdd180a512c82dca2c917778ae13827
anuLoveCoading <anusrivas07@gmail.com>
2022-09-24 16:54:38 +0530

    class

--- forLoop.java
+++ forLoop.java
@@ -14,17 +14,35 @@ public class forLoop {
 
         // Largest of N numbers;
 
-        Scanner sc= new Scanner(System.in);
+        // Scanner sc= new Scanner(System.in);
 
-        int n = sc.nextInt() ;
+        // int n = sc.nextInt() ;
 
-        int maxValue= Integer.MIN_VALUE;
+        // int maxValue= Integer.MIN_VALUE;
          
-        for (int i=1;i<=n;i++){
-            int current = sc.nextInt();
-            maxValue = Math.max(maxValue,current);
-        }
-        System.out.println(maxValue);
+        // for (int i=1;i<=n;i++){
+        //     int current = sc.nextInt();
+        //     maxValue = Math.max(maxValue,current);
+        // }
+        // System.out.println(maxValue);
 
+
+
+        // While loops;
+
+        int i=10;
+         int  count = 0;
+
+        while(  i<=50){
+
+            System.out.println(i);
+
+            i+=2;
+            count++;
+         }
+         System.out.println("********************");
+            System.out.println(count);
+
+        // i++;
     }
 }
