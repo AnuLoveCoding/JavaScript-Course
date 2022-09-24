23cecb1347337b2f57dd270b65ed9f02ed3e5198
anuLoveCoading <anusrivas07@gmail.com>
2022-09-24 15:59:12 +0530

    class

---
+++ forLoop.java
@@ -0,0 +1,13 @@
+/**
+ * forLoop
+ */
+public class forLoop {
+
+    public static void main(String[] args) {
+
+        for (int i=0 ; i<= 10; i+=5){
+
+            System.out.println(i);
+        }
+    }
+}
