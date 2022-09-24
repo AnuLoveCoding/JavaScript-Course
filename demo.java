10d3dcdd2ec0c0ec728910c53a765213e1658c4a
anuLoveCoading <anusrivas07@gmail.com>
2022-09-24 21:16:24 +0530

    class

---
+++ demo.java
@@ -0,0 +1,18 @@
+/**
+ * demo
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        
+
+        int num =20;
+        System.out.println(num+30);
+
+         num =50;
+ System.out.println(num);
+
+
+
+    }
+}
