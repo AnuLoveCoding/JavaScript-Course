71baeff7cb53156da4070413a2aaed0da5bf75fa
anuLoveCoading <anusrivas07@gmail.com>
2022-09-25 01:35:46 +0530

    class

---
+++ Array.java
@@ -0,0 +1,20 @@
+
+/**
+ * Array
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        /**Array :- Java array is a data structure that stores data of the same type in a sequential manner.An array takes a contiguous section of the memory. */
+
+            int marks1 =50;
+            int marks2=60;
+            int marks3=70;
+            int marks4 =80;
+            int marks5 =90;
+            int marks6 =40;
+            int marks7 =30;
+            int marks8 =20;
+
+    }
+}
--- demo.java
+++ demo.java
@@ -1,7 +1,8 @@
 /**
  * demo
  */
-public class demo {
+import java.util.* ;
+ class demo {
 
     public static void main(String[] args) {
         
@@ -12,12 +13,24 @@ public class demo {
 //          num =50;
 //  System.out.println(num);
 
-float new_number = 10.35f;
-  System.out.println(new_number);
+// float new_number = 10.35f;
+//   System.out.println(new_number);
 
 
-  double Aew_number = 10.36;
-  System.out.println(Aew_number);
+//   double Aew_number = 10.36;
+//   System.out.println(Aew_number);
+
+//   int a= 10,b=5;
+//   float c = (a*1.0f) / b;
+//   System.out.println(c);
+
+  Scanner sc = new Scanner(System.in);
+
+              long A = sc.nextLong();
+              long B= sc.nextLong();
+              System.out.print(A * B);
+            //   System.out.print(B);
+
 
 //   int x=10;
 //   System.out.println(x+1);
